a6e94002a55ff3573e58ca2e655ebb4e579212cf
livenow14 <xowjd41@naver.com>
2021-10-07 08:33:41 +0900

    1부 JUnit 5 - JUnit 5 테스트 이름 표시하기
    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
    보다 @DisplayName("스터디 만들기")을 조금더 많이 사용하는걸 추천

--- how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
+++ how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @BeforeAll
@@ -28,7 +29,8 @@ class StudyTest {
     }
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기")
+    void create_new_study() {
         //given
         final Study study = new Study();
         //when
@@ -39,7 +41,7 @@ class StudyTest {
 
     @Test
     @Disabled
-    void create1() {
+    void create_new_study_again() {
         //given
         final Study study = new Study();
         //when
