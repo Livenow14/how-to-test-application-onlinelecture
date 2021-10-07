244ab81e2d7c9c0e6014425ed779ec6702d246de
livenow14 <xowjd41@naver.com>
2021-10-07 10:02:56 +0900

    1부 JUnit 5 - JUnit 5 조건에 따라 테스트 실행하기

--- how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
+++ how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
@@ -1,6 +1,7 @@
 package com.livenow.howtotestapplicationonlinelecture;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
@@ -101,12 +102,47 @@ class StudyTest {
         /**
          * 100ms가 넘으면 바로 끊고 싶다면 assertTimeoutPreemptively 사용
          */
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+        assertTimeoutPreemptively(Duration.ofMillis(400), () -> {
             new Study(10);
             Thread.sleep(300);
         });
         // TODO ThreadLocal 문제
         // Spring에서 트랜잭션 설정을 가지고 있는 쓰레드와 별개의 쓰레드로 실행하기 때문에
         // 주의해서 사용해야함
+    }
+
+    @Test
+    @EnabledOnOs({OS.MAC, OS.LINUX})
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11 })
+    //@EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+    void create_test_with_condition() {
+        //given
+        final String test_env = System.getenv("TEST_ENV");
+        System.out.println("test_env = " + test_env);
+        //when
+        //then
+/*
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("local");
+            final Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("gump".equalsIgnoreCase(test_env), () -> {
+            System.out.println("gump");
+            final Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+*/
+    }
+
+    @Test
+    @DisabledOnOs(OS.MAC)
+    void create_test_with_condition_disable() {
+        //given
+        final String test_env = System.getenv("TEST_ENV");
+        System.out.println("test_env = " + test_env);
+        //when
     }
 }
