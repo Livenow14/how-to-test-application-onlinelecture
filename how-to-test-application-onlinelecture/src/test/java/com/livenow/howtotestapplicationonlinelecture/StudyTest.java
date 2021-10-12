a715f05af75cce417d24f48b3c10d571d420f84c
livenow14 <xowjd41@naver.com>
2021-10-12 23:50:51 +0900

    1부 JUnit 5 - JUnit 5 테스트 인스턴스

--- how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
+++ how-to-test-application-onlinelecture/src/test/java/com/livenow/howtotestapplicationonlinelecture/StudyTest.java
@@ -23,6 +23,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * - 테스트 클래스당 인스턴스를 하나만 만들어 사용한다.
+ * - 경우에 따라, 테스트 간에 공유하는 모든 상태를 @BeforeEach 또는 @AfterEach에서 초기화 할 필요가 있다.
+ * - @BeforeAll과 @AfterAll을 인스턴스 메소드 또는 인터페이스에 정의한 default 메소드로 정의할 수도 있다.
+ * @BeforeAll과 @AfterAll이 static을 붙이지 않아도 됨
+ * 
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
@@ -48,11 +56,20 @@ class StudyTest {
         System.out.println("after each - 테스트 이후 매번 실행");
     }
 
+    /**
+     * JUnit은 테스트 메소드 마다 테스트 인스턴스를 새로 만든다.
+     - 이것이 기본 전략.
+     - 테스트 메소드를 독립적으로 실행하여 예상치 못한 부작용을 방지하기 위함이다.
+     - 이 전략을 JUnit 5에서 변경할 수 있다.
+     * value 는 항상 1임
+     */
+    int value = 1;
+
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
         //given
-        final Study study = new Study(10);
+        final Study study = new Study(value++);
         //when
         //then
         assertNotNull(study);
@@ -63,6 +80,7 @@ class StudyTest {
     @Disabled
     void create_new_study_again() {
         //given
+        System.out.println("logger = " + logger);
         final Study study = new Study(10);
         //when
         //then
